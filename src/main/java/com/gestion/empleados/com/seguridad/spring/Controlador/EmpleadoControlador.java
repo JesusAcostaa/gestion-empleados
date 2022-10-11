3a05368dd620c55e9186d35194fea409aa86b253
JesusAcostaa <joacostaa@ufpso.edu.co>
2022-10-11 18:48:26 -0500

    Se agregan endpoints para la busqueda de empleado por ID y para la actualización de la información de un empleado

--- src/main/java/com/gestion/empleados/com/seguridad/spring/Controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/com/seguridad/spring/Controlador/EmpleadoControlador.java
@@ -1,8 +1,10 @@
 package com.gestion.empleados.com.seguridad.spring.Controlador;
 
+import com.gestion.empleados.com.seguridad.spring.Excepciones.ResourceNotFoundException;
 import com.gestion.empleados.com.seguridad.spring.Modelo.Empleado;
 import com.gestion.empleados.com.seguridad.spring.Repositorio.EmpleadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,4 +26,25 @@ public class EmpleadoControlador {
     public Empleado guardarEmpleado(@RequestBody Empleado empleado){
         return  repositorio.save(empleado);
     }
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> buscarEmpleadoPorId(@PathVariable Long id){
+        Empleado empleado = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el ID: " + id));
+        return ResponseEntity.ok(empleado);
+    }
+
+
+    @PutMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Long id, @RequestBody Empleado detallesEmpleado){
+        Empleado empleado = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el ID: " + id));
+        empleado.setNombre(detallesEmpleado.getNombre());
+        empleado.setApellido(detallesEmpleado.getApellido());
+        empleado.setEmail(detallesEmpleado.getEmail());
+        Empleado empleadoActualizado = repositorio.save(empleado);
+        return ResponseEntity.ok(empleadoActualizado);
+
+
+    }
+
 }
